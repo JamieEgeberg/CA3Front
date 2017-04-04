2a5d24f1245cd985b90c5a5bdc9c1d197e037f85
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-04-04 12:40:13 +0200

    Fixed ocd stuff in app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -14,33 +14,31 @@ const App = observer(class App extends Component {
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar-brand" href="#">Semester Seed</a>
+                            <a className="navbar-brand" href="#">Book  Store</a>
                         </div>
                         <ul className="nav navbar-nav">
                             <li><Link to="/home">Home</Link></li>
-                            <li><Link to="/documentation">Documentation</Link></li>
+                            <li><Link to="/documentation">Documentation</Link>
+                            </li>
                             <li><Link to="/products">Products</Link></li>
                             <li><Link to="/company">Company</Link></li>
-                            {auth.isUser ? <li><Link to="/books"> Add/Edit Books </Link></li> : null}
-                            {auth.isAdmin ? <li><Link to="/users"> Add/Edit Users </Link></li> : null}
+                            {auth.isUser ?
+                                <li><Link to="/books"> Add/Edit Books </Link>
+                                </li> : null}
+                            {auth.isAdmin ?
+                                <li><Link to="/users"> Add/Edit Users </Link>
+                                </li> : null}
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
-                            <li className="navbar-text" style={{color: "steelBlue"}}>{logInStatus}</li>
-                            <li>
-                                {auth.loggedIn ?
-                                    (
-                                        <Link to="/logout"><span className="glyphicon glyphicon-log-in"/>
-                                            Logout</Link>
-                                    ) :
-                                    (
-                                        <Link to="/login">
-                                            <span className="glyphicon glyphicon-log-out"/> Login </Link>
-                                    )}
-                            </li>
+                            {auth.loggedIn && (<li className="navbar-text" style={{color: "steelBlue"}}>{logInStatus}</li>)}
+                            {auth.loggedIn ?
+                                (<li><Link to="/logout"><span className="glyphicon glyphicon-log-out"/> Logout</Link></li>) :
+                                (<li><Link to="/login"><span className="glyphicon glyphicon-log-in"/> Login</Link></li>)}
                         </ul>
                     </div>
                 </nav>
-                {this.props.children || <p>You are {!auth.loggedIn && 'not'} logged in.</p>}
+                {this.props.children ||
+                <p>You are {auth.loggedIn || 'not'} logged in.</p>}
             </div>
         )
     }
