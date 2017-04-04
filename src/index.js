54519ebe2d89f7b89edf16b14c44f9af4ac53d07
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-04-04 10:07:40 +0200

    added details to routing

--- src/index.js
+++ src/index.js
@@ -7,35 +7,37 @@ import BooksPage from "./pages/BooksPage";
 import ProductsPage from "./pages/ProductsPage";
 import DocumentationPage from "./pages/DocumentationPage";
 import UsersPage from "./pages/UsersPage";
+import CompanyPage from "./pages/CompanyPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
+import Details from "./pages/Details";
 import Dashboard from "./pages/Dashboard";
 import auth from "./authorization/auth";
 
-
 function requireAuth(nextState, replace) {
-  if (!auth.loggedIn) {
-    replace({
-      pathname: '/login',
-      state: { nextPathname: nextState.location.pathname }
-    })
-  }
+    if (!auth.loggedIn) {
+        replace({
+            pathname: '/login',
+            state: {nextPathname: nextState.location.pathname}
+        })
+    }
 }
 
 ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="login" component={Login} />
-      <Route path="logout" component={Logout} />
-      <Route path="home" component={Home} />
-      <Route path="documentation" component={DocumentationPage} />
-      <Route path="products" component={ProductsPage} />
-      <Route path="company" component={CompanyPage} />
-      <Route path="books" component={BooksPage} />
-      <Route path="users" component={UsersPage} />
-      <Route path="dashboard" component={Dashboard} onEnter={requireAuth} />
-    </Route>
-  </Router>
-), document.getElementById('root'))
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="login" component={Login}/>
+            <Route path="logout" component={Logout}/>
+            <Route path="home" component={Home}/>
+            <Route path="documentation" component={DocumentationPage}/>
+            <Route path="products" component={ProductsPage}/>
+            <Route path="products/details/:id" component={Details} />
+            <Route path="company" component={CompanyPage}/>
+            <Route path="books" component={BooksPage}/>
+            <Route path="users" component={UsersPage}/>
+            <Route path="dashboard" component={Dashboard} onEnter={requireAuth}/>
+        </Route>
+    </Router>
+), document.getElementById('root'));
