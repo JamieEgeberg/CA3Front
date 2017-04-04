91c8f492d799ccbac84a8da2d6d244a461faacce
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-04-04 10:09:01 +0200

    added form for new book in bookspage

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {observer} from "mobx-react";
 import userData from "../stores/userStore";
+import bookStore from '../stores/BookStore';
 
 @observer
 class BooksPage extends Component {
@@ -13,23 +14,50 @@ class BooksPage extends Component {
         userData.getData();
     }
 
-    render() {
-        return (
-            <div>
-                <h2>Users</h2>
-                <p>This message is fetched from the server if you are properly logged in</p>
+    constructor() {
+        super();
+        this.state = {
+            book: {
+                title: "",
+                info: "",
+                moreInfo: "",
+            }
+        };
+    }
 
-                <ul>
-                    {userData.clubList.map((c) => {
-                        return <li>{c.name}, <a href={c.url}> Club page</a></li>
-                    })}
-                </ul>
-                {/*<div className="msgFromServer">*/}
-                {/*{userData.messageFromServer}*/}
-                {/*</div>*/}
-                {/*<h4 style={{ color: "red" }}>{userData.errorMessage}</h4>*/}
-            </div>
-        )
+    handleChange = (event) => {
+        var book = this.state.book;
+        var id = event.target.id;
+        if (id === "title") {
+            book.title = event.target.value;
+        } else if (id === "info") {
+            book.info = event.target.value;
+        } else if (id === "moreInfo") {
+            book.moreInfo = event.target.value;
+        }
+        this.setState(book);
+    };
+
+    saveBook = (event) => {
+        bookStore.addBook(this.state.book);
+        event.preventDefault();
+    };
+
+    render() {
+        return <div>
+            <h2>New Book</h2>
+            <form >
+                <input id="title" type="text" placeholder="Title" value={this.state.value}
+                       onChange={this.handleChange}/><br/>
+                <input id="info" type="text" placeholder="Info" value={this.state.value}
+                       onChange={this.handleChange}/><br/>
+                <input id="moreInfo" type="text" placeholder="More" value={this.state.value}
+                       onChange={this.handleChange}/><br/>
+                <button onClick={this.saveBook}>Add</button>
+                <p>{JSON.stringify(this.state.book)}</p>
+            </form>
+
+        </div>
     }
 }
 export default BooksPage;
