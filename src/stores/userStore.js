b7795ded6d78ece8d0082eec7b8f612036266c59
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-04-05 14:44:35 +0200

    AddUser added

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,4 +1,4 @@
-import {observable, action} from "mobx";
+import {computed, observable, action} from "mobx";
 import fetchHelper from "./fetchHelpers"
 const URL = require("../../package.json").serverURL;
 
@@ -6,19 +6,21 @@ const URL = require("../../package.json").serverURL;
 class UserStore {
     @observable messageFromServer = "";
     @observable errorMessage = "";
-    @observable clubList = [];
+    @observable _users = [];
 
+    constructor() {
+        this.getData();
+    }
 
     @action
     setErrorMessage = (err) => {
         this.errorMessage = err;
-    }
+    };
 
     @action
     getData = () => {
         this.errorMessage = "";
         this.messageFromServer = "";
-        this.clubList = [];
         let errorCode = 200;
         const options = fetchHelper.makeOptions("GET", true);
         fetch(URL + "api/footballclubs", options)
@@ -33,12 +35,48 @@ class UserStore {
                     throw new Error(`${res.error.message} (${res.error.code})`);
                 }
                 else {
-                    this.clubList = res;
+                    this._users = res;
                 }
             })).catch(err => {
             //This is the only way (I have found) to verify server is not running
             this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
         })
+    }
+
+    @computed get userCount() {
+        return this._users.length;
+    }
+
+    @computed get users() {
+        return this._users;
+    }
+
+    @action addUser(user) {
+        this.errorMessage = "";
+        this.messageFromServer = "";
+        let errorCode = 200;
+        let options = fetchHelper.makeOptions("POST", true);
+
+        options.body = JSON.stringify(user);
+        fetch(URL + "api/user", options)
+            .then((res) => {
+                if (res.status > 210 || !res.ok) {
+                    errorCode = res.status;
+                }
+                return res.json();
+            })
+            .then(action((res) => {  //Note the action wrapper to allow for useStrict
+                if (errorCode !== 200) {
+                    throw new Error(`${res.error.message} (${res.error.code})`);
+                }
+                else {
+                    this._users.push(res);
+                }
+            })).catch(err => {
+            //This is the only way (I have found) to verify server is not running
+            this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+        })
+
     }
 }
 
